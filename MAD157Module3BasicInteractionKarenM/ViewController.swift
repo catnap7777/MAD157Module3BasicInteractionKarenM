8f79a526682c95ad63224218af9c4483a31bd6bc
km <35552602+catnap7777@users.noreply.github.com>
2020-09-02 20:59:22 -0500

    initial commit

--- MAD157Module3BasicInteractionKarenM/ViewController.swift
+++ MAD157Module3BasicInteractionKarenM/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var textMessage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeButton(_ sender: UIButton) {
+        
+        labelResult.text = textMessage.text
+    }
+    
 }
 
